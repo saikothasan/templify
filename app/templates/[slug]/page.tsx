f2cca424bc6f4ec75e4ec91579165d353135d919
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-07-16 03:16:48 +0600

    Update page.tsx

--- app/templates/[slug]/page.tsx
+++ app/templates/[slug]/page.tsx
@@ -5,6 +5,8 @@ import TemplateDetailPageClient from "./TemplateDetailPageClient"
 import { getTemplateBySlugAction, getRelatedTemplatesAction } from "@/app/actions/template-actions"
 import { getAllTemplateSlugs } from "@/lib/templates" // Import from new utility
 
+export const runtime = "edge"
+
 // For SSG: generateStaticParams now uses the new utility to get slugs
 export async function generateStaticParams() {
   return getAllTemplateSlugs()
